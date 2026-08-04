d5631d6a091d85732b185c701ff15e7350332fc6
Rodean Moradi <rodean.moradi@gmail.com>
2026-08-03 21:48:41 -0400

    Removed centroid encoder from model architecture and created model training loop

--- src/model.py
+++ src/model.py
@@ -1,35 +1,23 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from src.baseline import RegressionHead
 from src.resnet import init_resnet
 from src.pointnet import PointNetBackbone
 
-
-class CentroidEncoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.layer_1 = nn.Linear(3, 64)
-
-    def forward(self, centroid):
-
-        return F.relu(self.layer_1(centroid))
 
 class PoseEstimator(nn.Module):
     def __init__(self):
         super().__init__()
         self.resnet = init_resnet()
         self.pointnet = PointNetBackbone()
-        self.encoder = CentroidEncoder()
         self.embedding = nn.Embedding(num_embeddings=10, embedding_dim=16)
-        self.head = RegressionHead(input_size=1616, output_size=9) # 512 from resnet, 64 from encoder, 16 from embedding, 1024 from pointnet; 3 for translation, 6 for rotation
+        self.head = RegressionHead(input_size=1552, output_size=9) # 512 from resnet, 16 from embedding, 1024 from pointnet; 3 for translation, 6 for rotation
 
-    def forward(self, rgb, pointcloud, centroid, obj_id):
+    def forward(self, rgb, pointcloud, obj_id):
         appearance_feats = self.resnet(rgb)
         geometry_feats = self.pointnet(pointcloud)
-        centroid_encoding = self.encoder(centroid)
         object_embedding = self.embedding(obj_id)
-        feats = torch.cat([object_embedding, appearance_feats, geometry_feats, centroid_encoding], dim=1)
+        feats = torch.cat([object_embedding, appearance_feats, geometry_feats], dim=1)
         out = self.head(feats)
 
         return out
--- src/train.py
+++ src/train.py
@@ -11,10 +11,10 @@ def get_translation(out, geom):
 
     return t
 
-def gram_schmidt(out):
+def gram_schmidt(out, rotation_start_index):
     epsilon = 1.0e-8
-    # Head outputs a tensor of size 7 (first dims translation, last 6 rotation) TODO: Update indices and add var
-    rotation_6d = out[:, 1:]
+    # Head outputs a tensor of size 7 or 9 (first 1 or 3 dims translation, last 6 rotation)
+    rotation_6d = out[:, rotation_start_index:]
     a = rotation_6d[:, :3]
     b = rotation_6d[:, 3:]
 
@@ -62,7 +62,7 @@ def train_baseline(model, ne, bs, lr):
             out = model(rgb, geom, obj_id)
             
             t = get_translation(out, geom)
-            rot = gram_schmidt(out)
+            rot = gram_schmidt(out, rotation_start_index=1)
 
             translation_loss = translation_criterion(t, translation_gt)
             rotation_loss = rotation_criterion(rot, rotation_gt)
@@ -95,7 +95,7 @@ def train_baseline(model, ne, bs, lr):
                 out = model(rgb, geom, obj_id)
                 
                 t = get_translation(out, geom)
-                rot = gram_schmidt(out)
+                rot = gram_schmidt(out, rotation_start_index=1)
     
                 translation_loss = translation_criterion(t, translation_gt)
                 rotation_loss = rotation_criterion(rot, rotation_gt)
@@ -125,6 +125,94 @@ def train_baseline(model, ne, bs, lr):
     return
 
 # TODO: Determine metrics/results needed
-def train():
+def train(model, ne, bs, lr):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = torch.device(device)
+
+    model.to(device)
+    translation_criterion = nn.SmoothL1Loss(beta=0.01)
+    rotation_criterion = nn.MSELoss() # Chordal loss
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    train_loader, val_loader, _ = build_dataloader(bs)
+    loss_lambda = 0.5
+    best_val_loss = 1.0e9
+    for e in range(ne):
+        model.train()
+        train_t_loss_running = 0.0
+        train_rot_loss_running = 0.0
+        train_loss_running = 0.0
+        for b in train_loader:
+            optimizer.zero_grad()
+
+            rgb = b["rgb"].to(device)
+            pointcloud = b["pointcloud"].to(device)
+            obj_id = b["obj_id"].to(device)
+            centroid = b["centroid"].to(device)
+            translation_gt = b["translation_m2c"].to(device)
+            rotation_gt = b["rotation_m2c"].to(device)
+
+            out = model(rgb, pointcloud, obj_id)
+            
+            t = out[:, :3] + centroid
+            rot = gram_schmidt(out, rotation_start_index=3)
+
+            translation_loss = translation_criterion(t, translation_gt)
+            rotation_loss = rotation_criterion(rot, rotation_gt)
+            train_loss = translation_loss + loss_lambda * rotation_loss
+
+            train_t_loss_running += translation_loss.item()
+            train_rot_loss_running += rotation_loss.item()
+            train_loss_running += train_loss.item()
+
+            train_loss.backward()
+
+            optimizer.step()
+
+        train_t_loss_running /= len(train_loader)
+        train_rot_loss_running /= len(train_loader)
+        train_loss_running /= len(train_loader)
+
+        model.eval()
+        val_t_loss_running = 0.0
+        val_rot_loss_running = 0.0
+        val_loss_running = 0.0
+        with torch.no_grad():
+            for b in val_loader:
+                rgb = b["rgb"].to(device)
+                pointcloud = b["pointcloud"].to(device)
+                obj_id = b["obj_id"].to(device)
+                centroid = b["centroid"].to(device)
+                translation_gt = b["translation_m2c"].to(device)
+                rotation_gt = b["rotation_m2c"].to(device)
+    
+                out = model(rgb, pointcloud, obj_id)
+                
+                t = out[:, :3] + centroid
+                rot = gram_schmidt(out, rotation_start_index=3)
+    
+                translation_loss = translation_criterion(t, translation_gt)
+                rotation_loss = rotation_criterion(rot, rotation_gt)
+                val_loss = translation_loss + loss_lambda * rotation_loss
+
+                val_t_loss_running += translation_loss.item()
+                val_rot_loss_running += rotation_loss.item()
+                val_loss_running += val_loss.item()
+
+            val_t_loss_running /= len(val_loader)
+            val_rot_loss_running /= len(val_loader)
+            val_loss_running /= len(val_loader)
+
+            if val_loss_running < best_val_loss:
+                best_val_loss = val_loss_running
+
+                checkpoint = {
+                    "model_state_dict": model.state_dict(),
+                    "loss": best_val_loss,
+                    "epoch": e
+                }
+                torch.save(checkpoint, f"data/models/best_ne_{ne}_bs_{bs}_lr_{lr}.pt")
+
+        print(f"Epoch: {e}, Training Loss (Total): {train_loss_running}, Training Loss (Rotation): {train_rot_loss_running}, Training Loss (Translation): {train_t_loss_running}")
+        print(f"Epoch: {e}, Validation Loss (Total): {val_loss_running}, Validation Loss (Rotation): {val_rot_loss_running}, Validation Loss (Translation): {val_t_loss_running}")
 
     return
